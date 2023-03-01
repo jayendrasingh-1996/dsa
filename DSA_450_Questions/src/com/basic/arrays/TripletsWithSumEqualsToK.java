57a97e4e92975a16bf3f7d5e7ac7871e3678e875
jayendrasingh-1996 <jayendrathakur1996@gmail.com>
2023-03-02 02:00:06 +0530

    Triplet Sum in Array

--- DSA_450_Questions/src/com/basic/arrays/TripletsWithSumEqualsToK.java
+++ DSA_450_Questions/src/com/basic/arrays/TripletsWithSumEqualsToK.java
@@ -1,9 +1,73 @@
 package com.basic.arrays;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class TripletsWithSumEqualsToK {
 
-		 public static void main(String[] args) {
+	public static void main(String[] args) {
+
+		int[] arr = { 1, 4, 45, 6, 10, 8 };
+
+		int k = 14;
+
+		boolean res = find3Numbers(arr,  arr.length,k);
+		System.out.println(res);
+	}
+
+	
+	// this is using 2 pointer methods
+	public static boolean find3Numbers(int A[], int n, int X) {
+
+		// Your code Here
+		
+		Arrays.sort(A);
+		
+		for(int i = 0 ; i < n ; i++) {
 			
-			 System.out.println("Hello");
+			int y = X -A[i];
+			
+			int low = i+1;
+			int high = n-1;
+			
+			
+			while(low < high) {
+				
+				if(A[low] + A[high] == y ) {
+					return true;
+				}else if( A[low] + A[high] >y ) {
+					 high--;
+				}else {
+					low++;
+				}
+				
+			}
+			
 		}
+		
+		
+		return false;
+
+	}
+
+	
+	// this is the brute force approach 
+	public static boolean findTriplets(int[] arr, int x, int n) {
+
+		for (int i = 0; i < n; i++) {
+
+			for (int j = i + 1; j < n; j++) {
+
+				for (int k = j + 1; k < n; k++) {
+
+					if (arr[i] + arr[j] + arr[k] == x) {
+						return true;
+
+					}
+				}
+			}
+		}
+
+		return false;
+	}
 }
